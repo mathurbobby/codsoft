53f286d7471872c95ca04aba58428b601ea869b0
mathurbobby <104289331+mathurbobby@users.noreply.github.com>
2023-08-10 18:30:54 +0530

    Update SignUp.jsx

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -32,7 +32,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // try {
-    const response = await fetch("http://localhost:5000/api/createuser", {
+    const response = await fetch("https://clickcart-9q8w.onrender.com/api/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
