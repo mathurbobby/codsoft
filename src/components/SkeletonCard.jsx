6d7678406915b625a215c500293028831bde9ff8
mathurbobby <104289331+mathurbobby@users.noreply.github.com>
2023-08-11 23:06:28 +0530

    Add files via upload

---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import Card from "react-bootstrap/Card";
+
+const SkeletonCard = () => {
+  return (
+    <>
+      <Card
+        className="mycard shadow-lg"
+        style={{ width: "18rem", maxHeight: "360px" }}
+      >
+        <div style={{ width: "100%", height: "150px", objectFit: "fill" }}
+        className="p-2" ><Skeleton height={130} /></div>
+        <Card.Body>
+          <Card.Title>
+            {" "}
+            <Skeleton />
+          </Card.Title>
+          <div className="w-100">
+            {/* <span>
+              {" "}
+              <Skeleton />
+            </span>
+            <span>
+              {" "}
+              <Skeleton />
+            </span> */}
+            <div className=" h-100 fs-5">
+              {" "}
+              <Skeleton />
+            </div>
+            <hr />
+            <div className="w-100">
+              <Skeleton />
+            </div>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default SkeletonCard;
